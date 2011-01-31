8647135f8986b8c5c5c2aed397fe7cfecb745ccc
Stephen Swift <stephen.swift@gmail.com>
2011-01-31 02:51:23 -0500

    Changed @include metadata in plugin

--- yt_comment_plus.js
+++ yt_comment_plus.js
@@ -4,7 +4,7 @@
 // @description	  Extends the YouTube comment section to include a page goto form. I cannot imagine a single person beside myself ever needing this.
 // @author        Stephen Swift
 // @homepage      https://github.com/rap/YouTube-Comment-/raw/master/yt_comment_plus.js
-// @include       http://*youtube.com*
+// @include       *youtube.com*
 // ==/UserScript==
 
 var $;
